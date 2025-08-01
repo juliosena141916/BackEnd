9c11e40c1c459d2d8789c0ead02a8a8243ccd1b5
julio <juliosena.1411@gmail.com>
2025-08-01 20:49:25 -0300

    01.08.25

--- Aula01/Variaveis.php
+++ Aula01/Variaveis.php
@@ -1,3 +1,9 @@
 <?php
-      echo"Manuel Gomes"
+      echo"Olá!\n";
+      $nome = "Júlio Sena\n";
+      $idade = 19;
+      $ano_atual = 2025;
+
+      $data_nasc = $ano_atual - $idade;
+      echo $nome, "você nasceu em:", $data_nasc;
 ?>
