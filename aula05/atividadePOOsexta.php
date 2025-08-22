b43d38706a1a3b30f979c5a9b852c93040ea9e62
julio <juliosena.1411@gmail.com>
2025-08-22 18:33:16 -0300

    samambaia

---
+++ aula05/atividadePOOsexta.php
@@ -0,0 +1,61 @@
+<?php
+// class Carro
+// {
+//     public $marca;
+//     public $modelo;
+//     public $ano;
+//     public $revisao;
+//     public $n_donos;
+
+
+//     public function __construct($marca, $modelo, $ano, $revisao, $n_donos)
+//     {
+//         $this->marca = $marca;
+//         $this->modelo = $modelo;
+//         $this->ano = $ano;   
+//         $this->revisao = $revisao;
+//         $this->n_donos = $n_donos;
+//     }
+
+//     public function exibirInfo(){
+//         echo "Marca: $this->marca - Modelo> $this->modelo - Ano: $this->ano\n";
+//     }
+
+//     public function ligar(){
+//         echo "O carro $this->modelo está ligado\n";
+//     }
+// }
+
+
+// $carro1 = new Carro("Porsche", "911", "2020", false, 3);
+// $carro2 = new Carro("Volkswagen", "Gol", "2000", true, 6 );
+// $carro3 = new Carro("Chevrolet", "Classic", "2002", false, 2 );
+// $carro4 = new Carro("Fiat", "Uno", "2010", true, 5 );
+// $carro5 = new Carro("Chevrolet", "Corsa", "2008", false, 2 );
+// $carro6 = new Carro("Volkswagen", "Polo", "1990", false, 2 );
+ 
+// $carro1 -> ligar();
+// $carro4 -> exibirInfo();
+
+
+class cachorro{
+    public $nome;
+    public $raca;
+    public $sexo;
+    public $idade;
+
+    public function __construct($nome, $raca, $sexo, $idade){
+        $this -> nome = $nome;
+        $this -> raca = $raca;
+        $this -> sexo = $sexo;
+        $this -> idade = $idade;
+    }
+
+    public function latir(){
+        echo "O cachorro $nome está latindo";
+    }
+}
+
+$cachorro1 = new cachorro(Roberto, bulldog, M, 5);
+
+$cachorro1 -> latir();
