aa7de456607895784bc9d1d598a4082a0dffe593
julio <juliosena.1411@gmail.com>
2025-08-27 16:40:30 -0300

    pp

--- Aula07/getter_setter.php
+++ Aula07/getter_setter.php
@@ -9,24 +9,48 @@ class Pessoa{
     private $senha;
 
     public function __construct($nome, $cpf, $telefone, $idade, $email, $senha){
-        $this -> nome = $nome;
-        $this -> cpf = $cpf;
-        $this -> telefone = $telefone;
-        $this -> idade = $idade;
+        $this -> setNome($nome);
+        $this -> setCpf($cpf);
+        $this -> setTelefone($telefone);
+        $this -> setIdade($idade);
         $this -> email = $email;
         $this -> senha = $senha;
     }
 
         public function setNome($nome){
-        $this->nome = ucwords(strtolower)($nome);
+        $this->nome = ucwords(strtolower($nome)
+        );
     }
 
     public function getNome(){
         return $this->nome;
     }
 
-    
+    public function setCpf($cpf){
+        $this->cpf = preg_replace('/\D/', '', $cpf);
+    }
 
+    public function getCpf(){
+        return $this->nome;
+    }
 
+    public function setTelefone($telefone){
+        $this->telefone = preg_replace('/\D/', '', $telefone);
+    }
 
+    public function getTelefone(){
+        return $this->telefone;
+    }
+
+    public function setIdade($idade){
+        $this->idade = abs((int)$idade);
+    }
+
+    public function getIdade(){
+        return $this->idade;
+    }
 }
+
+$aluno1 = new Pessoa("JulIO seNa", "123.456.789-10", "(19)98821-4246", -20, "teste@teste.com", "testetes12");
+echo $aluno1->getNome();
+
