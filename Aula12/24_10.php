bc59f2e3bda6d7e41b5f0cc910fb159951db5797
julio <juliosena.1411@gmail.com>
2025-10-24 18:34:52 -0300

    pp

---
+++ Aula12/24_10.php
@@ -0,0 +1,10 @@
+<?php
+
+// array associativo
+// É um array onde se associa uma chave a um valor, aonde as chaves assumem o papel de indice do array. Exemplo:
+
+$alunos ["nome"] = "Gabrielson";
+
+//Neste exemplo o valor "Gabrielson" está associado à chave "nome".
+//Isso é feito para melhorar o entendimento do código e na execução e manipulação do array. Caso fossemos trabalhar com um array convencional,
+//  teriamos que lembrar qual o indice que iria corresponder à chave "nome"
--- Aula12/AlunoDAO.php
+++ Aula12/AlunoDAO.php
@@ -3,9 +3,43 @@
 class alunoDAO{
 
 private $alunos = [];
+private $arquivo = "alunos.json";
 
+public function __construct(){
+    if (file_exists($this->arquivo)){
+        $conteudo = file_get_contents($this->arquivo);
+        $dados = json_decode($conteudo, true);
+
+        if($dados){
+            foreach($dados as $id => $info){
+                $this->alunos[$id] = new Aluno(
+                    $info['id'],
+                    $info['nome'],
+                    $info['curso']
+                );
+            }
+        }
+    }
+}
+
+private function SalvaremArquivo(){
+    $dados = [];
+
+    foreach ($this->alunos as $id => $aluno){
+        $dados[$id]=[
+            'id' => $aluno->getId(),
+            'nome' => $aluno->getNome(),
+            'curso' => $aluno->getCurso()
+        ];
+    }
+
+    file_put_contents($this->arquivo, json_encode($dados, JSON_PRETTY_PRINT));
+}
+
+//CREATE
 public function criarAluno(Aluno $aluno){
     $this->alunos[$aluno->getId()]= $aluno;
+    $this->SalvaremArquivo();
 }
 
 public function lerAluno(){
@@ -17,10 +51,12 @@ public function atualizarAluno($id, $novoNome, $novoCurso){
         $this->alunos[$id]->setNome(nome: $novoNome);
         $this->alunos[$id]->setCurso(curso: $novoCurso);
     }
+    $this->SalvaremArquivo();
 }
 
 public function excluirAluno($id){
     unset($this->alunos[$id]);
+    $this->SalvaremArquivo();
 }
 
 }
